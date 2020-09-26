0abcfcdfa27010322ac24d20eed6aa6d78d1d1ac
maksiess <maksi95kg@gmail.com>
2020-09-26 02:44:28 +0000

    Added Flaskex.groovy and Spring-petclinic.groovy for Jenkins

---
+++ flaskex.groovy
@@ -0,0 +1,31 @@
+properties([
+    parameters([
+        string(defaultValue: '', description: 'Input node IP', name: 'SSHNODE', trim: true)
+        ])
+    ])
+
+node {
+    withCredentials([sshUserPrivateKey(credentialsId: 'Jenkins-Master', keyFileVariable: 'SSHKEY', passphraseVariable: '', usernameVariable: 'SSHUSERNAME')]) {
+        stage("Initialize") {
+            sh "ssh -o StrictHostKeyChecking=no -i $SSHKEY $SSHUSERNAME@${ params.SSHNODE } yum install epel-release -y "
+        }
+        stage("Install git") {
+            sh "ssh -o StrictHostKeyChecking=no -i $SSHKEY $SSHUSERNAME@${ params.SSHNODE } yum install git -y"
+        }
+        stage("Install pip 3.6") {
+            sh "ssh -o StrictHostKeyChecking=no -i $SSHKEY $SSHUSERNAME@${ params.SSHNODE } rm -rf Flaskex*"
+        }
+        stage("Download git repo") {
+            sh "ssh -o StrictHostKeyChecking=no -i $SSHKEY $SSHUSERNAME@${ params.SSHNODE } git clone https://github.com/anfederico/Flaskex && ls"
+        }
+        stage("Install python3") {
+            sh "ssh -o StrictHostKeyChecking=no -i $SSHKEY $SSHUSERNAME@${ params.SSHNODE } yum install python3 python3-pip3 -y"
+        }
+        stage("Install requirements") {
+            sh "ssh -o StrictHostKeyChecking=no -i $SSHKEY $SSHUSERNAME@${ params.SSHNODE } pip3 install -r Flaskex/requirements.txt"
+        }
+        stage("Install app.py") {
+            sh "ssh -o StrictHostKeyChecking=no -i $SSHKEY $SSHUSERNAME@${ params.SSHNODE } 'python3 Flaskex/app.py &'"
+        }
+    }
+}
---
+++ spring-petclinic.groovy
@@ -0,0 +1,35 @@
+properties([
+    parameters([
+        string(defaultValue: '', description: 'Input node IP', name: 'SSHNODE', trim: true)
+        ])
+    ])
+
+node {
+    withCredentials([sshUserPrivateKey(credentialsId: 'Jenkins-Master', keyFileVariable: 'SSHKEY', passphraseVariable: '', usernameVariable: 'SSHUSERNAME')]) {
+        stage("Initialize") {
+            sh "ssh -o StrictHostKeyChecking=no -i $SSHKEY $SSHUSERNAME@${ params.SSHNODE } yum install epel-release -y "
+        }
+        stage("Install java") {
+            sh "ssh -o StrictHostKeyChecking=no -i $SSHKEY $SSHUSERNAME@${ params.SSHNODE } yum install java-1.8.0-openjdk-devel -y"
+            
+        }
+        stage("Install git") {
+            sh "ssh -o StrictHostKeyChecking=no -i $SSHKEY $SSHUSERNAME@${ params.SSHNODE } yum install git -y"
+        }
+        stage("Removing repo") {
+            sh "ssh -o StrictHostKeyChecking=no -i $SSHKEY $SSHUSERNAME@${ params.SSHNODE } rm -rf spring-petclinic*"
+        }
+        stage("Download repo") {
+            sh "ssh -o StrictHostKeyChecking=no -i $SSHKEY $SSHUSERNAME@${ params.SSHNODE } git clone https://github.com/spring-projects/spring-petclinic.git"
+        }
+        stage("Moving to the folder") {
+            sh "ssh -o StrictHostKeyChecking=no -i $SSHKEY $SSHUSERNAME@${ params.SSHNODE } 'cd spring-petclinic/ && ./mvnw package'"
+        }
+        stage("Rename Java") {
+            sh "ssh -o StrictHostKeyChecking=no -i $SSHKEY $SSHUSERNAME@${ params.SSHNODE } 'cd spring-petclinic/ && mv target/*.jar target/app.jar'"
+        }
+        stage("Run JAVA") {
+            sh "ssh -o StrictHostKeyChecking=no -i $SSHKEY $SSHUSERNAME@${ params.SSHNODE } java -jar spring-petclinic/target/app.jar"
+        }
+    }
+}
