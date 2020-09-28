adb01c0f0d7350d38eb7435ed8fe1323229c8316
maksiess <maksi95kg@gmail.com>
2020-09-28 07:01:06 +0000

    updated

--- ansible-spring-petclinic.groovy
+++ ansible-spring-petclinic.groovy
@@ -1,30 +1,38 @@
 properties([
     parameters([
-        string(defaultValue: '', description: 'Provide Node IP', name: 'SSHNODE', trim: true)
+        string(defaultValue: '', description: 'Provide Node IP', name: 'node', trim: true)
         ])
     ])
-node {
-    stage("Pull Repo") {
-        git url: 'https://github.com/maksiess/jenkins-ansible-spring-petclinic.git'
+    
+node{
+    stage("Pulling GitHub YML Files"){
+        git url: 'https://github.com/maksiess/Ansible-Spring-Petclinic.git'
     }
-    stage("Install Prerequisites"){
-        ansiblePlaybook become: true, colorized: true, credentialsId: 'Jenkins-Master', disableHostKeyChecking: true, inventory: "${params.node}", playbook: 'prerequisites.yml'
+    stage("Installing Prerequisites"){
+        ansiblePlaybook become: true, colorized: true, credentialsId: 'Jenkins-Master', disableHostKeyChecking: true, inventory: "${params.node},", playbook: 'prerequisites.yml'
     }
-    withEnv(['PETCLINIC_REPO=https://github.com/maksiess/jenkins-ansible-spring-petclinic.git', 'PETCLINIC_BRANCH=master']) {
-    stage("Installing-Java") {
-        ansiblePlaybook become: true, colorized: true, credentialsId: 'Jenkins-Master', disableHostKeyChecking: true, inventory: "${params.node}", playbook: 'Install-Java.yml'
+    withEnv(['PETCLINIC_REPO=https://github.com/ikambarov/spring-petclinic', 'PETCLINIC_BRANCH=master']) {
+        stage("Pulling Repository"){
+            ansiblePlaybook become: true, colorized: true, credentialsId: 'Jenkins-Master', disableHostKeyChecking: true, inventory: "${params.node},", playbook: 'Pull-Repo.yml'
+        }
     }
-    stage("Installing Maven") {
-        ansiblePlaybook become: true, colorized: true, credentialsId: 'Jenkins-Master', disableHostKeyChecking: true, inventory: "${params.node}", playbook: 'Install-Maven.yml'
+    
+    stage("Installing-Java"){
+            ansiblePlaybook become: true, colorized: true, credentialsId: 'Jenkins-Master', disableHostKeyChecking: true, inventory: "${params.node},", playbook: 'Install-Java.yml'
+        }
+    stage("Installing Maven"){
+        ansiblePlaybook become: true, colorized: true, credentialsId: 'Jenkins-Master', disableHostKeyChecking: true, inventory: "${params.node},", playbook: 'Install-Maven.yml'
     }
-    stage("Changing-App-Name") {}
-        ansiblePlaybook become: true, colorized: true, credentialsId: 'Jenkins-Master', disableHostKeyChecking: true, inventory: "${params.node}", playbook: 'Changing-App-Name.yml'
+    stage("Changing-App-Name"){
+        ansiblePlaybook become: true, colorized: true, credentialsId: 'Jenkins-Master', disableHostKeyChecking: true, inventory: "${params.node},", playbook: 'Changing-App-Name.yml'
     }
-    stage("Starting Java") {
-        ansiblePlaybook become: true, colorized: true, credentialsId: 'Jenkins-Master', disableHostKeyChecking: true, inventory: "${params.node}", playbook: 'start-app-java.yml'
+    stage("Starting App"){
+        ansiblePlaybook become: true, colorized: true, credentialsId: 'Jenkins-Master', disableHostKeyChecking: true, inventory: "${params.node},", playbook: 'Start-App.yml'
     }
-    stage("Starting All Apps") {
-        ansiblePlaybook become: true, colorized: true, credentialsId: 'Jenkins-Master', disableHostKeyChecking: true, inventory: "${params.node}", playbook: 'start-app-all.yml'
+    stage("Starting Java"){
+        ansiblePlaybook become: true, colorized: true, credentialsId: 'Jenkins-Master', disableHostKeyChecking: true, inventory: "${params.node},", playbook: 'start-app-java.yml'
     }
-
+    stage("Starting All Apps"){
+        ansiblePlaybook become: true, colorized: true, credentialsId: 'Jenkins-Master', disableHostKeyChecking: true, inventory: "${params.node},", playbook: 'start-app-all.yml'
+    }
 }
